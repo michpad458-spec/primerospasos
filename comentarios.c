d51e3d1d8ce9c49750d027ac2b188cb29aa824d0
michpad458-spec <michpad458@>
2026-05-07 19:34:04 +0200

    test

---
+++ comentarios.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    // Este programa muestra un mensaje
+    //mi primer programa con comentarios
+    //michelle sarahi echeverria padilla
+    //hoy es dia jueves, 7 de mayo de 2026
+    //este ejercicio trata sobre como poner comentarios en el codigo
+
+    printf("Estoy aprendiendo comentarios\n");
+
+    /*
+       Este comentario
+       ocupa varias líneas
+    */
+
+    printf("Fin del programa\n");
+
+    return 0;
+}
---
+++ variables1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int edad = 18;
+    int edad = 30;
+    int edad = 45;
+
+    printf("La edad es: %d\n", edad);
+
+    return 0;
+}
