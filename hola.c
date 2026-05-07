7f86b37a5dc67d550e1e2592f80c4a6803caf1dc
michpad458-spec <michpad458@gmail.com>
2026-05-07 17:03:32 +0200

    hoy es dia jueves segundo intento

---
+++ hola.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+//asi se crea un comentario dentro del codigo
+//mi primer programa que dice hola mundo
+printf("hoy es dia jueves"); // esta es la funcion que imprime en pantalla
+//necesito indicar el tipo de dato que va a devolver la funcion main
+return 0; //el valor de retorno 0 indica que el programa termino correctamente
+}
---
+++ mi-primera-suma.c
@@ -0,0 +1,9 @@
+//tipos de datos numericos enteros
+#include <stdio.h>  
+int main() {
+    int a = 5; //declaracion de una variable entera
+    int b = 10; //declaracion de otra variable entera
+    int suma = a + b; //operacion de suma entre las dos variables
+    printf("La suma de %d y %d es: %d", a, b, suma); //imprime el resultado de la suma en pantalla
+    return 0; //indica que el programa termino correctamente
+}
