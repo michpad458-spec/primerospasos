5c0a49d9ce792793999e47735e0bcbab8712701d
michpad458-spec <michpad458@>
2026-05-07 18:48:04 +0200

    test ok

--- saludos.c
+++ saludos.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 
-    printf("Bienvennnidos al curso de C\n");
+    printf("Bienvennnnnidos al curso de C\n");
     printf("Estamos aprendiendo variables\n");
     printf("Y compilando programas\n");
     return 0;
