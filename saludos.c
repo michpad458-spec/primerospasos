9b497fb5d6a2864acf879c78903be5e775ed468a
michpad458-spec <michpad458@>
2026-05-07 18:34:51 +0200

    testeando

---
+++ saludos.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+
+    printf("Bienvenidos al curso de C\n");
+    printf("Estamos aprendiendo variables\n");
+    printf("Y compilando programas\n");
+    return 0;
+}
