bf39e31954590d5e91bb347d2b1898c8ff44b398
michpad458-spec <michpad458@>
2026-05-08 19:18:12 +0200

    variables numericas

---
+++ reto_final.c
@@ -0,0 +1,16 @@
+//este codigo es mi reto final
+//nombre:michelle padilla
+//edad:19 años
+//ciudad:madrid
+//altura:1.52
+#include <stdio.h>
+
+int main() {
+    printf("hola, mi nombre es michelle padilla\n");
+    printf("tengo 19 años\n");
+    printf("vivo en madrid");
+    printf("mido 1.52 metros\n");
+
+    return 0; 
+}
+
--- variables2.c
+++ variables2.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main() {
-
-    float altura = 70.5;
-
+//declaracion de una variable tipo float (numero que puede tener decimales)
+//definir el tipo de dato, nombre de la variable + el valor de la variable
+//ENTRADA DE DATOS
+    float altura = 1.75;//valor de la variable altura
+    float peso = 70.5;// valor de la variable peso
+//procesamiento de datos tratamientos de datos
     printf("La altura es: %f\n", altura);
-
+    printf("El peso es: %f\n", peso);
+    printf("La altura es: %f y el peso es: %f", altura, peso);
+//salida de datos
     return 0;
 }
