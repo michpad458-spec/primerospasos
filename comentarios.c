13e963f05580305ec624f1f5c77efb2e5e2342a4
michpad458-spec <michpad458@>
2026-05-08 18:15:26 +0200

    tarea variables1

--- comentarios.c
+++ comentarios.c
@@ -1,21 +1,23 @@
+//ESTE CODIGO ES UN PROGRAMA
+//michelle padilla
+//hoy es dia viernes 8 de mayo de 2026
+//este archivo se llama comentarios.c
 #include <stdio.h>
 
 int main() {
+// entrada de datos
 
     // Este programa muestra un mensaje
-    //mi primer programa con comentarios
-    //michelle sarahi echeverria padilla
-    //hoy es dia jueves, 7 de mayo de 2026
-    //este ejercicio trata sobre como poner comentarios en el codigo
-
+//ESTE PROGRAMA EJECUTA LA ACCION DE IMPRESION EN PANTALLA
+// tratamiento de datos
     printf("Estoy aprendiendo comentarios\n");
-
+// este comentario es una sola linea
     /*
        Este comentario
        ocupa varias líneas
     */
-
+// salida de datos
     printf("Fin del programa\n");
-
+// si hago un cambio en el archivo origen
     return 0;
 }
--- variables1.c
+++ variables1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-
-    int edad = 18;
-    int edad = 30;
-    int edad = 45;
-
-    printf("La edad es: %d\n", edad);
-
+// DECLARACION de una VARIABLE TIPO int (interger) es numerica y es un numero entero sin decimales
+// TIPO DE VARIALE + NOMBRE DELA VARIABLE + VALOR DE LA VARIABLE
+//entrada de datos
+    int numeroAlumnos = 20; // PUNTO Y COMA PARA FINALIZAR LA SENTENCIA
+    //procesamiento de datos
+    //funcion imprimir en pantalla con una opcion dentro del texto
+    //para sustituir por el valor de la variable se usa el simbolo % seguido de la letra que corresponde al tipo de dato
+    printf("La edad es: %d\n", numeroAlumnos); 
+//salida de datos
     return 0;
 }
